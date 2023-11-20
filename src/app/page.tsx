3ab3ee6aefa5d55d237377f7f006fbbe7ecd8f71
javi <javier.bravoz@mail.udp.cl>
2023-11-20 14:32:22 -0300

    prueba numero 1000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const HomePage: React.FC = () => {
     return (
         <div className="flex flex-col gap-4 rounded-lg border-2 border-white p-4 justify-center items-center backdrop-blur-md bg-blue/50">            
-            <Link href="/caja" passHref>
+            <Link href="/caja">
                 <button className="text-white border-2 border-white rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300">
                     control de caja
                 </button>
