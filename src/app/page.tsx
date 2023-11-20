df5c922e06f7cf8c75169f51d0c177f3ca9585c1
javi <javier.bravoz@mail.udp.cl>
2023-11-20 15:34:48 -0300

    update de links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 const HomePage: React.FC = () => {
     return (
         <div className="flex flex-col gap-4 rounded-lg border-2 border-white p-4 justify-center items-center backdrop-blur-md bg-blue/50">            
-            <li className='list-none pl-2 text-white border-2 border-white rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300'>
+            <h2 className='list-none pl-2 text-white border-2 border-white rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300'>
             <Link href="/caja">
                 control de caja
             </Link>
-            </li>
-            <li className='list-none pl-2 text-white border-2 border-white rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300' >
+            </h2>
+            <h2 className='list-none pl-2 text-white border-2 border-white rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300' >
             <Link href="/inventario">
                 inventario   
             </Link>
-            </li>
+            </h2>
         </div>
     );
 };
