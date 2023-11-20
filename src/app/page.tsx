e99230d2914a13ebbda78d0c92040d44fd5eb63a
javi <javier.bravoz@mail.udp.cl>
2023-11-20 11:57:38 -0300

    legacybehavior agregado a los links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,15 @@ const HomePage: React.FC = () => {
     return (
         
         <div className="flex flex-col gap-4 rounded-lg border-2 border-white-500 p-4 justify-center items-center backdrop-blur-md bg-blue/50">            
-            <Link href="/caja" passHref>
-            <a>
+            <Link href="/caja" legacyBehavior>
                 <button className="text-white border-2 border-white-500 rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300">
                     control de caja
                 </button>
-            </a>
             </Link>
-            <Link href="/inventario" passHref>
-            <a>
+            <Link href="/inventario" legacyBehavior>
                 <button className="text-white border-2 border-white-500 rounded-lg p-2 hover:scale-90 hover:shadow-md hover:shadow-fuchsia-200 transition duration-300">
                     inventario
-                </button>
-            </a>    
+                </button>    
             </Link>
         </div>
     );
